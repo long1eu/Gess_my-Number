4f46763108fb483fe1369917adbf5bb2fbda543a
osan petrut <baichestie@gmail.com>
2020-11-18 12:33:03 -0800

    guess_number

---
+++ lib/main.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'test.dart';
+
+void main() {
+  runApp(Guess());
+}
+class Guess extends StatefulWidget {
+  @override
+  _GuessState createState() => _GuessState();
+}
+class _GuessState extends State<Guess> {
+  final guessedNumber = new TextEditingController();
+  final rng = new Random();
+  String display = ' ';
+  String guess1 = ' ';
+
+  void ourAns() {
+    numberRandom = numberRandom;
+    print(guessedNumber.text);
+    int guess = int.parse(guessedNumber.text);
+
+    if (guess > 100 || guess < 1) {
+      setState(() {
+        display = 'Choose number between 1 and 100';
+      });
+      guessedNumber.clear();
+    }
+    if (guess > numberRandom) {
+      setState(() {
+        display = ("Try a LOWER number!");
+      });
+      guessedNumber.clear();
+    } else if (guess < numberRandom) {
+      setState(() {
+        display = ("Try a HIGHER number!");
+      });
+      guessedNumber.clear();
+    } else {
+     setState(() {
+       display = ("That is right.");
+     });
+    }
+    print(display);
+    guess1 = guess.toString();
+    //return guess;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Guess my number'),
+          centerTitle: true,
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'lib/img/nr.jpg',
+              ),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text(
+                      'I\'m thinking of a number between 1 and 100. ',
+                      style: TextStyle(
+                        fontSize: 17.0,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text(
+                      'It\'s your turn to guess my number ! ',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text(
+                      'Your number is $guess1',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      display,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              Card(
+                color: Colors.blue,
+                child: Column(
+                  children: [
+                    Text(
+                      'Select your number ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    TextField(
+                      textAlign: TextAlign.left,
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          ),
+                        ),
+                        labelText: ('Your ans is...'),
+                        hintStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      keyboardType: TextInputType.number,
+                      controller: guessedNumber,
+                    ),
+                    FlatButton(
+                      child: Text(
+                        'Guess',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                        ),
+                      ),
+                      color: Colors.blue,
+                      onPressed: () {
+                        print(guess1);
+                        ourAns();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/test.dart
@@ -0,0 +1,6 @@
+import 'dart:math';
+
+
+var rng = new Random();
+int numberRandom = rng.nextInt(100);
+
